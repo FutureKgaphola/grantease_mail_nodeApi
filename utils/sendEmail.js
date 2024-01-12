8f38674f4abf4a719ad5d26c44f613632d869a7a
FutureKgaphola <futurekgaphola@gmail.com>
2024-01-12 17:45:11 +0000

    grantease mail sender backend

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,8 +5,8 @@ module.exports.senduserMail = async (email,name,message,subject) => {
 		const transporter = nodemailer.createTransport({
 			host: process.env.HOST,
 			service: process.env.SERVICE,
-			port: Number(process.env.EMAIL_PORT),
-			secure: Boolean(process.env.SECURE),
+			port: 587,
+			secure: true,
 			auth: {
 				user: process.env.USER,
 				pass: process.env.PASS,
