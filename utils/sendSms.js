51d698992e33873206450c326ed18406d9c20b42
FutureKgaphola <futurekgaphola@gmail.com>
2024-01-19 14:14:10 +0000

    added sms sending

--- utils/sendSms.js
+++ utils/sendSms.js
@@ -9,7 +9,7 @@ const from = "GrantEase"
 const to = "27738285578"
 
 module.exports.sendSMS=async(paydate)=> {
-    const text = `GrantEase. Your next expected grant payment date - ${String(paydate)}.\n\n.`;
+    const text = `GrantEase.\n Your next expected grant payment date - ${String(paydate)}.\n\n.`;
     await vonage.sms.send({to, from, text})
         .then(resp => { console.log('Message sent successfully'); console.log(resp); })
         .catch(err => { console.log('There was an error sending the messages.'); console.error(err); });
